4648a788684d916c8e89c1d99497a8fb03bf0fe6
gunnar durant <gdurant1@my.scccd.edu>
2026-09-12 13:13:25 -0700

    step 4 complete

--- w6a/app.js
+++ w6a/app.js
@@ -1,6 +1,7 @@
 import {getOrderInputs} from "./order-handler.js";
 import * as priceCalculator from "./price-calculator.js"; //import
 import * as resultDisplay from './results-display.js';
+import * as orderStorage from './order-storage.js';
 
 const orders = []; //initialize array
 
@@ -30,8 +31,15 @@ const handleOrderSubmit = function (event) {
 const init = function () {
     // attached submit event listener
     formElement.addEventListener("submit", handleOrderSubmit);
-
     console.log("App Initialized");
+
+    const loadOrders = orderStorage.loadOrders();
+    if(loadOrders > 0){
+        orders.push(...loadOrders);
+        console.log('Entries loaded localStorage') }
+    else {
+        console.log('No entries found in localStorage, starting fresh')
+    }
 }
 
 document.addEventListener('DOMContentLoaded', init);{}
